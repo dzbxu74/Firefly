9b9855780c6bb5395dff6a60bb02c354a7896566
dzbxu74 <dzbxiu1@163.com>
2026-08-04 02:03:53 +0000

    modified:   src/assets/images/avatar.avif 	modified:   src/config/profileConfig.ts 	modified:   src/config/siteConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "修丁有什么坏心思呢",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "这个修丁有满满的坏心思呐～",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
