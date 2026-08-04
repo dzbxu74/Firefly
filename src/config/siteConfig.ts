2da0d95e1233508d4b8bec2d2a9014ca172a3645
dzbxu74 <dzbxiu1@163.com>
2026-08-04 09:32:57 +0000

    new file:   public/favicon/kafei.png 	deleted:    src/assets/images/favicon/kafei.avif 	modified:   src/config/siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -56,7 +56,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "assets/images/favicon/kafei.avif",
+			src: "/favicon/kafei.png",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
