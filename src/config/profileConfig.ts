ef7d2ab78cc04e771e13e91baf8787913410476f
dzbxu74 <dzbxiu1@163.com>
2026-08-04 05:10:34 +0000

    modified:   src/config/profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -24,19 +24,19 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "https://qm.qq.com/q/TdKJzmvGU0",
 			showName: false,
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/dzbxu74",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:dzbxiu1@163.com",
 			showName: false,
 		},
 		{
